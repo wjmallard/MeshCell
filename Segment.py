1fa06ff16f87366e3076e135c965f348249f444c
William Mallard <wmallard@g.harvard.edu>
2020-04-28 13:08:12 -0700

    Find boundaries of cells.
    Need to order boundary coords along the contour.
    Matlab's bwboundaries() does this automatically.

--- Segment.py
+++ Segment.py
@@ -12,6 +12,8 @@ from skimage import io
 from skimage import util
 from skimage.morphology import reconstruction
 from skimage.morphology import watershed
+from skimage.measure import regionprops
+from skimage.segmentation import find_boundaries
 
 FILENAME = '/Users/wmallard/Desktop/Microscopy/test.tif'
 
@@ -38,3 +40,28 @@ def fill_image_to_min_height(image, h_min):
 
 im_filled = fill_image_to_min_height(im, h_min_to_fill)
 object_labels = watershed(im_filled, connectivity=8)
+
+#%% Step 2: Fit Contours
+
+def get_cell_boundary_coords(object_labels, boundaries, cell_id):
+    '''
+    Find the boundary coordinates of a particular cell.
+
+    Arguments:
+        object_labels - int matrix; labels which object each pixel belongs to
+        boundaries - bool matrix; labels whether a pixel is a boundary
+        cell_id - specifies which object to find the boundary coords of
+    '''
+    cell_mask = object_labels == cell_id
+    cell_boundary = boundaries & cell_mask
+
+    props = regionprops(cell_boundary.astype(int))
+    assert len(props) == 1
+    p = props[0]
+
+    X = p.coords.T[0]
+    Y = p.coords.T[1]
+
+    return X, Y
+
+boundaries = find_boundaries(object_labels)
