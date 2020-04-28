90e746aea470fc5defeff99bfd67ba7f27e171ed
William Mallard <wmallard@g.harvard.edu>
2020-04-28 13:14:27 -0700

    Port interpcurv2n() from C to Python.
    This is the core of of Morphometrics' subpixel fitting algorithm.
    Proper use of Numpy vectorized math should be just as fast as C.
    
    The core of interpcurv2n() is interp1c().
    This calls locate(), which uses binary search.
    Rewrote locate() using Numpy broadcasting and matrix logic.

--- Segment.py
+++ Segment.py
@@ -3,11 +3,12 @@
 """
 Created on Sun Apr 26 23:42:35 2020
 
-@author: wmallard
+@author: wmallard xies
 """
 
 #%% Step 0: Load libraries and image.
 
+import numpy as np
 from skimage import io
 from skimage import util
 from skimage.morphology import reconstruction
@@ -65,3 +66,44 @@ def get_cell_boundary_coords(object_labels, boundaries, cell_id):
     return X, Y
 
 boundaries = find_boundaries(object_labels)
+
+# TODO: Pre-compute smoothed image force components.
+
+# TODO: Loop over all cells.
+#for cell_id in range(num_cells):
+#    pass
+cell_id = 2
+
+X, Y = get_cell_boundary_coords(object_labels, boundaries, cell_id)
+
+# Interpolate along curve to obtain evenly spaced spline anchors.
+#
+# X -- cartesian x-coordinates
+# Y -- cartesian y-coordinates
+# C -- 1D path coordinate along the contour
+#
+# NOTE: Assumes X-Y coordinates are sorted along the contour.
+X_old, Y_old = X, Y
+
+# Compute old coordinates along the contour with uneven spacing.
+dx = X_old[1:] - X_old[:-1]
+dy = Y_old[1:] - Y_old[:-1]
+C_old = np.cumsum(np.sqrt(dx**2 + dy**2))
+
+# Create new coordinates along the contour with even 1-pixel spacing.
+C_length = np.int(np.ceil(C_old[-1]))
+C_new = np.arange(C_length)
+
+# Find the distance between each new (evenly spaced) anchor point
+# and the next-smallest old (unevenly spaced) anchor point.
+anchor_distance = C_old[:,None] - C_new[None,:]
+anchor_distance[anchor_distance > 0] = -np.inf
+
+# For each new anchor point,
+# find the index of the next-smallest old anchor point.
+J = np.argmax(anchor_distance, axis=0)
+
+# Update step.
+alpha = (C_new - C_old[J]) / (C_old[J+1] - C_old[J])
+X_new = X_old[J] + (X_old[J+1] - X_old[J]) * alpha
+Y_new = Y_old[J] + (Y_old[J+1] - Y_old[J]) * alpha
