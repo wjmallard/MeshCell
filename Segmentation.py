fe5a68503a2205ff331f57a8f8fe0999d51a4fb1
William Mallard <wmallard@g.harvard.edu>
2020-05-16 22:51:04 -0700

    Add phase image segmentation.
    Contrast stretch, gradient, fill, watershed.

--- Segmentation.py
+++ Segmentation.py
@@ -11,6 +11,27 @@ from skimage import segmentation
 
 h_min_to_fill = 0.1  # relative to 1.0 maximum
 
+def contrast_stretch(im, clip=2):
+    '''
+    Enhance image contrast via linear scaling.
+
+    Stretch pixel values to cover the full dynamic range of the image format.
+    Optionally clip pixel values at a given percentile, saturating any values
+    below the Xth percentile or above the (100-X)th percentile.
+
+    Parameters
+    ----------
+    im : 2D np.array, float32
+        A TIFF image to be segmented.
+    clip : scalar, float32
+        Percentile for clipping, in [0, 100).
+
+    '''
+    assert(0 <= clip < 100)
+    v_min, v_max = np.percentile(im, (clip, 100 - clip))
+    im = exposure.rescale_intensity(im, in_range=(v_min, v_max))
+    return im
+
 def fill_image_to_min_height(im, h_min):
     '''
     Reproduces Matlab's imhmin() function.
@@ -29,6 +50,27 @@ def fill_image_to_min_height(im, h_min):
     im = morphology.reconstruction((im - h_min), im, method='dilation')
     im = util.invert(im)
     return im
+
+def segment_phase_image(im):
+    '''
+    Segment a phase image via watershed.
+
+    Parameters
+    ----------
+    im : 2D np.array, float32
+        A TIFF image to be segmented.
+
+    Returns
+    -------
+    object_labels : 2D np.array, int32
+        Assigns each pixel to a cell_id.
+
+    '''
+    im = contrast_stretch(im)
+    im = filters.sobel(im_stretch)
+    im = fill_image_to_min_height(im, h_min_to_fill)
+    object_labels = segmentation.watershed(im, connectivity=8)
+    return object_labels
 
 def segment_deepcell_masks(im):
     '''
