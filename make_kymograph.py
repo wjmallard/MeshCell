2754374b10027957f6b3a383a3700fda6f86bc71
William Mallard <wmallard@g.harvard.edu>
2020-05-17 02:19:05 -0700

    Generate a kymograph at every mesh rib.
    Use RectBivariateSpline to interpolate along each rib.

--- make_kymograph.py
+++ make_kymograph.py
@@ -7,15 +7,19 @@
 """
 from skimage import io
 import matplotlib.pyplot as plt
+from scipy.interpolate import RectBivariateSpline
 
 import Segmentation
 import Contour
 import Skeleton
 import Mesh
 
-FILENAME = 'test_phase.tif'
+PHASE = 'test_phase.tif'
+TIRF_MIP = 'test_tirf_mip.tif'
+TIRF_REG = 'test_tirf_reg.tif'
 
-im = io.imread(FILENAME, as_gray=True)
+#%% Generate meshes for phase image.
+im = io.imread(PHASE, as_gray=True)
 object_labels = Segmentation.segment_phase_image(im)
 
 Contours = Contour.ContourGenerator(im, object_labels)
@@ -25,7 +29,7 @@ Contours = Contour.ContourGenerator(im, object_labels)
 # for cell_id in range(num_cells):
 #     X, Y = get_cell_boundary_coords(object_labels, cell_id)
 #     if X is None: continue
-cell_id = 4
+cell_id = 3
 
 contour = Contours.generate(cell_id)
 skeleton = Skeleton.generate(contour)
@@ -45,3 +49,31 @@ for i in range(len(rib_starts)):
 
     plt.plot([xs, xt], [ys, yt], 'b')
     plt.plot([xs, xb], [ys, yb], 'b')
+
+#%% Generate kymographs along meshes.
+movie = io.imread(TIRF_REG)
+
+num_points = 20
+
+Sy, Sx = movie[0].shape
+x_mesh = np.arange(Sx)
+y_mesh = np.arange(Sy)
+
+for i in range(len(top_intersections)):
+
+    print(f'Processing rib {i}.')
+
+    x1, y1 = top_intersections[i]
+    x2, y2 = bot_intersections[i]
+
+    x_points = np.linspace(x1, x2, num_points)
+    y_points = np.linspace(y1, y2, num_points)
+
+    F = [RectBivariateSpline(y_mesh, x_mesh, frame) for frame in movie]
+
+    K = np.array([f.ev(y_points, x_points) for f in F])
+
+    plt.close('all')
+    plt.imshow(K, cmap='gray')
+    plt.title(f'Rib {i}')
+    plt.savefig(f'Rib_{i:02}.png')
