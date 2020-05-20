7c2cd900c2a83eb6c7e84188d9c8fa717aecb102
William Mallard <wmallard@g.harvard.edu>
2020-05-20 00:54:24 -0700

    Loop over all cells and calculate velocities.

--- make_kymograph.py
+++ make_kymograph.py
@@ -7,7 +7,6 @@
 """
 import numpy as np
 from skimage import io
-import matplotlib.pyplot as plt
 from scipy.interpolate import RectBivariateSpline
 from scipy.signal import find_peaks
 
@@ -15,48 +14,17 @@ import Segmentation
 import Contour
 import Skeleton
 import Mesh
+import Kymograph
 
 PHASE = 'test_phase.tif'
-TIRF_MIP = 'test_tirf_mip.tif'
 TIRF_REG = 'test_tirf_reg.tif'
+TIRF_MIP = 'test_tirf_mip.tif'
 
-#%% Generate meshes for phase image.
-im = io.imread(PHASE, as_gray=True)
-object_labels = Segmentation.segment_phase_image(im)
-
-bg_id = Segmentation.identify_background(im, object_labels, im_type='phase')
-
-Contours = Contour.ContourGenerator(im, object_labels)
-
-# TODO: Loop over all cells.
-# for cell_id in num_cells.unique():
-#     if cell_id == bg_id: continue
-#     X, Y = get_cell_boundary_coords(object_labels, cell_id)
-cell_id = 3
-
-contour = Contours.generate(cell_id)
-skeleton = Skeleton.generate(contour)
-rib_starts, top_intersections, bot_intersections = Mesh.make_ribs(contour, skeleton)
-
-
-plt.close('all')
-plt.imshow(im, cmap='gray')
-plt.plot(*contour.T, 'ko-')
-plt.plot(*skeleton.T, 'rx-')
-
-for i in range(len(rib_starts)):
-
-    xs, ys = rib_starts[i]
-    xt, yt = top_intersections[i]
-    xb, yb = bot_intersections[i]
-
-    plt.plot([xs, xt], [ys, yt], 'b')
-    plt.plot([xs, xb], [ys, yb], 'b')
-
-#%% Generate kymographs along meshes.
+phase = io.imread(PHASE)
 movie = io.imread(TIRF_REG)
 tirf_mip = io.imread(TIRF_MIP)
 
+# Prepare kymograph interpolation.
 num_points = 20
 
 Sy, Sx = movie[0].shape
@@ -66,61 +34,68 @@ y_mesh = np.arange(Sy)
 interp_mip = RectBivariateSpline(y_mesh, x_mesh, tirf_mip)
 interp_movie = [RectBivariateSpline(y_mesh, x_mesh, frame) for frame in movie]
 
-# Find intensity peaks along skeleton.
-rib_sums = []
+# Generate meshes for phase image.
 
-for P1, P2 in zip(top_intersections, bot_intersections):
+# Segment cells.
+object_labels = Segmentation.segment_phase_image(phase)
+bg_id = Segmentation.identify_background(phase, object_labels, im_type='phase')
 
-    x1, y1 = P1
-    x2, y2 = P2
+# Prepare contour generator.
+Contours = Contour.ContourGenerator(phase, object_labels)
 
-    x_points = np.linspace(x1, x2, num_points)
-    y_points = np.linspace(y1, y2, num_points)
+Velocities = []
 
-    result = interp_mip.ev(y_points, x_points).sum()
-    rib_sums.append(result)
+for cell_id in np.unique(object_labels):
 
-rib_sums = np.array(rib_sums)
-peaks, _ = find_peaks(rib_sums, height=rib_sums.mean())
+    print(f'Processing cell {cell_id}.')
 
-for i in peaks:
+    if cell_id == bg_id:
+        print(' - Background, skipping.')
+        continue
 
-    print(f'Processing cell {cell_id} rib {i}.')
+    contour = Contours.generate(cell_id)
+    skeleton = Skeleton.generate(contour)
+    mesh = Mesh.make_ribs(contour, skeleton)
+    rib_starts, top_intersections, bot_intersections = mesh
 
-    # Construct rib.
-    x1, y1 = top_intersections[i]
-    x2, y2 = bot_intersections[i]
+    # Find intensity peaks along skeleton.
+    rib_sums = []
 
-    x_points = np.linspace(x1, x2, num_points)
-    y_points = np.linspace(y1, y2, num_points)
+    for P1, P2 in zip(top_intersections, bot_intersections):
 
-    # Generate kymograph.
-    K = np.array([interp_frame.ev(y_points, x_points)
-                  for interp_frame in interp_movie])
+        x1, y1 = P1
+        x2, y2 = P2
 
-    # Plot.
-    plt.close('all')
-    fig = plt.figure(figsize=(9, 8), constrained_layout=True)
-    fig.suptitle(f'Cell {cell_id} Rib {i}')
-    grid = fig.add_gridspec(2, 2,
-                            wspace=0.0,
-                            hspace=0.0,
-                            width_ratios=[10, 1],
-                            height_ratios=[1, 50])
+        x_points = np.linspace(x1, x2, num_points)
+        y_points = np.linspace(y1, y2, num_points)
 
-    ax = fig.add_subplot(grid[1,0])
-    ax.imshow(tirf_mip, cmap='gray')
-    ax.plot((x1, x2), (y1, y2), 'r-', linewidth=2)
-    ax.plot(*contour.T, 'g:')
-    ax.plot(*skeleton.T, 'y:')
-    ax.set_xlim([0, Sx])
-    ax.set_ylim([Sy, 0])
-    ax.set_xticks([])
-    ax.set_yticks([])
+        result = interp_mip.ev(y_points, x_points).sum()
+        rib_sums.append(result)
 
-    ax = fig.add_subplot(grid[1,1])
-    ax.imshow(K, cmap='gray')
-    ax.set_xticks([])
-    ax.set_yticks([])
+    rib_sums = np.array(rib_sums)
+    peaks, _ = find_peaks(rib_sums, height=rib_sums.mean())
 
-    plt.savefig(f'Cell_{cell_id}_Rib_{i:04}.png')
+    # Generate a kymograph at each peak.
+    for i in peaks:
+
+        print(f'Processing cell {cell_id} rib {i}.')
+
+        # Construct rib.
+        x1, y1 = top_intersections[i]
+        x2, y2 = bot_intersections[i]
+
+        x_points = np.linspace(x1, x2, num_points)
+        y_points = np.linspace(y1, y2, num_points)
+
+        # Generate kymograph.
+        K = np.array([interp_frame.ev(y_points, x_points)
+                      for interp_frame in interp_movie])
+
+        # Calculate velocity.
+        sampling_period = 1  # second
+        cell_diameter = 11 * .065  # microns
+        velocity = Kymograph.calculate_velocity(K, sampling_period, cell_diameter)
+
+        Velocities.append(velocity)
+
+Velocities = np.array(Velocities)
